33f83eddc0699d43eeadc546b8fba855495a6994
Sayan Ghosh <sggsayan93@gmail.com>
2015-02-07 21:09:52 -0800

    Redirect calls

--- run.py
+++ run.py
@@ -1,15 +1,46 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from __future__ import with_statement   # Only necessary for Python 2.5
 import twilio.twiml
  
 app = Flask(__name__)
  
+ 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming requests."""
+    from_number = request.values.get('From', None)
+    caller = "Monkey"
+ 
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    # Greet the caller by name
+    resp.say("Hello ")
+    # Play an mp3
+    resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
+ 
+    # Say a command, and listen for the caller to press a key. When they press
+    # a key, redirect them to /handle-key.
+    with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
+        g.say("To speak to a real person, press 1. Press any other key to start over.")
  
     return str(resp)
+ 
+@app.route("/handle-key", methods=['GET', 'POST'])
+def handle_key():
+    """Handle key press from a user."""
+ 
+    # Get the digit pressed by the user
+    digit_pressed = request.values.get('Digits', None)
+    if digit_pressed == "1":
+        resp = twilio.twiml.Response()
+        # Dial (310) 555-1212 - connect that number to the incoming caller.
+        resp.dial("+14088584926")
+        # If the dial fails:
+        resp.say("The call failed, or the remote party hung up. Goodbye.")
+ 
+        return str(resp)
+ 
+    # If the caller pressed anything but 1, redirect them to the homepage.
+    else:
+        return redirect("/")
  
 if __name__ == "__main__":
     app.run(debug=True)
